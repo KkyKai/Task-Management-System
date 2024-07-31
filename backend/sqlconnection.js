3642deb69820c03af458c1e5f91fd89161a4a6e6
KkyKai <kaiyangthegreat@gmail.com>
2024-07-31 08:26:23 +0800

    Update sqlconnection.js

--- backend/sqlconnection.js
+++ backend/sqlconnection.js
@@ -1,24 +1,23 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
-const express = require('express');
+const express = require("express");
 
 // create a new MySQL connection
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  //password: 'root',
-password: 'kaiyang123',
-  database: 'nodelogin'
+  host: "localhost",
+  user: "root",
+  password: "root",
+  //password: 'kaiyang123',
+  database: "nodelogin",
 });
 
 // connect to the MySQL database
-connection.connect((error)=>{
-    if(error) {
-        console.error('Error connecting to MySQL database:', error);
-    }
-    else {
-        console.log('Connected to MySQL database as id!', connection.threadId);
-    }
+connection.connect((error) => {
+  if (error) {
+    console.error("Error connecting to MySQL database:", error);
+  } else {
+    console.log("Connected to MySQL database as id!", connection.threadId);
+  }
 });
 
 module.exports = connection;
